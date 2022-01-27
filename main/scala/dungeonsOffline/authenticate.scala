f968ca36298a342d3d11886a62814806de07dfef
Grace Alberts <grace.alberts@revature.net>
2022-01-27 09:01:38 -0600

    MVP commit

--- main/scala/dungeonsOffline/authenticate.scala
+++ main/scala/dungeonsOffline/authenticate.scala
@@ -1,12 +1,47 @@
+import org.apache.spark.sql.execution.SQLExecution
+import java.sql.DriverManager
+import java.sql.Connection
+
 object authenticate {
     def stringBuilder(buf: Array[Char]):String = {
+        
         var result = ""
         buf.foreach(x => result += x.toString())
         return result;
     }
 
-    def authUser(user:String, pass:Array[Char]): Boolean = {
-        // TODO: do this
+    def authUser(user:String, pass:String): Boolean = {
+        val driver = "com.mysql.cj.jdbc.Driver"
+        val url = "jdbc:mysql://localhost/project0"
+        val user= "root"
+        val password = "pass1"       
+        //note this is a password for a test DB that i have. You should NEVER use a password this insecure.
+        //you should also never log in using root, and use a different user profile.
+        var connection:Connection = null
+        try {
+            Class.forName(driver)
+            connection = DriverManager.getConnection(url,user,password)
+            println("Connected to Database")
+        } catch {
+            
+            case e:Throwable => {println("Failed tp connect to the database. Program is quiting"); e.printStackTrace}
+        }
+        
+        val username2 = "\"" + user + "\""
+        val passString = "\"" + pass+ "\""
+        val querys = s"SELECT * FROM playerdata WHERE username = $username2 AND password = $passString"
+        val action = connection.createStatement()
+        action.execute(querys)
+        val result = action.getResultSet()
+        var back = ""
+        while(result.next()){
+            back = result.getString("id")
+        }
+        //didnt find a match
+        println(back)
+        if (back!= null) {
         return true;
+        }
+        return false        
   }
 }
--- main/scala/dungeonsOffline/mainInput.scala
+++ main/scala/dungeonsOffline/mainInput.scala
@@ -9,6 +9,7 @@ import authenticate._
  
 class Monster(rawid:Int,rawname:String,rawhealth:Int,rawattack:Int, rawdefense:Int, diff:String){
     //the Monster Class, for all your Monster Needs.
+    val name = rawname;
     val id = rawid //This is a VAL cause the id of the monster should never change.
     var health = rawhealth
     var attack = rawattack
@@ -55,18 +56,58 @@ class Player(user:String, uid:Int){
     def createDungeon(mon:Monster, gen:Generator): Unit={
         dungeon = new Dungeon(0, mon,gen)
     }
+
+    def createDungeon(mon:Map[Monster, Int], gen:Map[Generator, Int], en:Double):Unit = {
+        createDungeon(null,null)
+        dungeon.monsters = mon
+        dungeon.gens = gen
+        dungeon.energy = en        
+    }
+
     def formatMonstersForSaving():String= {
         var tempDun = ""
-        var temp = dungeon.monsters.foreach((x:(Monster, Int)) => tempDun += (x._1.id.toString +"," + x._2.toString + "|"))
+        var temp = dungeon.monsters.foreach((x:(Monster, Int)) => tempDun += (x._1.id.toString +"," + x._2.toString + "&"))
         return tempDun
     }
     def formatGeneratorForSaving():String={
         var tempDun = ""
-        var temp = dungeon.gens.foreach((x:(Generator, Int)) => tempDun += (x._1.id.toString +"," + x._2.toString + "|"))
+        var temp = dungeon.gens.foreach((x:(Generator, Int)) => tempDun += (x._1.id.toString +"," + x._2.toString + "&"))
         return tempDun
     }
     def getEnergyLevel(): Double = {
         return dungeon.energy
+    }
+    def getDungeonStatus():String = {
+        //returns a preformatted String toprint out a stus of the dungeon in the console.
+        var finalOut = "+-----------------------------------+\n"
+        finalOut +=    "| Monsters:                         |\n"
+        
+        var mName = "| "
+        dungeon.monsters.foreach(t => (mName += t._1.name + ": " + t._2.toString()))
+        while(mName.length < 36) {
+            mName += " "
+        }
+        mName +="|\n"
+        finalOut += mName;
+        finalOut +=    "| Generators:                       |\n"
+        mName = "| "
+        dungeon.gens.foreach(t => (mName += t._1.name + ": " + t._2.toString()))
+        while(mName.length < 36) {
+            mName += " "
+        }
+        mName +="|\n"
+        finalOut += mName
+        mName = "| Energy: " + getEnergyLevel().toString()
+        while(mName.length < 36) {
+            mName += " "
+        }
+        mName +="|\n"
+        finalOut += mName
+        finalOut += "+-----------------------------------+\n"
+        return finalOut
+    }
+    def tick(){
+        dungeon.energy += 1.5;
     }
 }
 
@@ -90,17 +131,38 @@ object mainInput {
             
             case e:Throwable => {println("Failed tp connect to the database. Program is quiting"); e.printStackTrace}
         }
+
+    
 // ==========================================================================================================================================
         println("Welcome to Dungeons Offline! TO log in, type \"login\". To make and account, type \"new\". To quit, type \"quit\"")
         //i dont like using while true loops but i will fix it later
         var res = "";
         var player:Player = null;
+        
         do { 
             val input = readLine()
             input.toLowerCase() match {
-                case "quit" => System.exit(0)
+                case "quit" => {connection.close();System.exit(0)}
                 case "login" => res = "login"
-                case "new" => println("Account creation in not availible at this time") //TODO: account creation
+                case "new" => {println("Welcome, you are making a new character. Please enter a username (case-sensitive)")
+                                 val newUser ="\"" + readLine("Username:") + "\""
+                                println("Please enter a password (case-sensitive)")
+                                val pass ="\"" + readLine("Password:") + "\""
+                                println("Creating account...")
+                                val statement1 = connection.createStatement()
+                                val emptyJSON = "NULL"
+                                val query1 = "SELECT id FROM playerdata ORDER BY id DESC LIMIT 1"
+                                statement1.execute(query1)
+                                var resulting= statement1.getResultSet()
+                                var i = 0;
+                                while(resulting.next()){
+                                i = resulting.getInt("id") + 1
+                                }
+                                val query2 = s"INSERT INTO playerData VALUES ($i,$newUser,$pass);"
+                                val query3 = s"INSERT INTO dungeondata VALUES ($i, $emptyJSON , 0, $emptyJSON);"
+                                statement1.execute(query2)
+                                statement1.execute(query3)
+                                println("New player created. Please login.")}
                 case _ => println("Please enter an acceptable input.")
             }
         }
@@ -113,7 +175,7 @@ object mainInput {
             var breaker = true;
             while(currentTries < maxTries && breaker){
                 val user = readLine("Username:")
-                val pass = stdIn.readPassword("Password")
+                val pass = stdIn.readLine("Password:").toString()
                 if(!authenticate.authUser(user,pass)){
                     currentTries+=1;
                     println("Incorrect Username or Password, please try again. Remainging tries: " + (maxTries-currentTries).toString())
@@ -135,8 +197,10 @@ object mainInput {
                 println("You have run out of password attempts. Program quiting.")
                 System.exit(0);
             }
+        } else if (res == "new"){
+            
         } else {
-            //TODO: new account creation
+            println(" :) ")
         }
 //=======================================================================================================================================================
         println("Welcome! Checking if you have a dungeon...")
@@ -182,9 +246,106 @@ object mainInput {
             } else {
                 //This is for if the dungeon already exists.
                 //TODO: get and reformat that
-                println(":)")
+                //pull down the data about the player's dungeon
+                val getData = connection.createStatement()
+                val playerID= player.getId()
+                val dungeonQuery = s"SELECT * FROM dungeondata WHERE dungeonid = $playerID"
+                getData.execute(dungeonQuery)
+                val res = getData.getResultSet()
+                var monsterString = ""
+                var genString = ""
+                var energy = 0.0
+                while(res.next()){
+                    monsterString = res.getString("monsterdata")
+                    genString = res.getString("generators")
+                    energy = res.getDouble("energy")
+                }
+                //not that we have it from the database, we need to parse it
+                var splitMonsterData = monsterString.split("&")
+                var splitGenData= genString.split("&")
+                var monstersToGet = new ArrayBuffer[Int]();
+                var monstersAmounts = new ArrayBuffer[Int]();
+                var tempMonsterMap = Map[Monster,Int]();
+                //usually, I would need to do the same thing twice for the genrators, but since im using a temp, i dont need too >:)
+                //now we have two arrays of strings 
+                for(i<-0 until splitMonsterData.length){
+                    var tempbuffer = splitMonsterData(i).split(",")
+                    monstersToGet += tempbuffer(0).toInt
+                    monstersAmounts += tempbuffer(1).toInt
+                    //ok so this should do it.
+                }
+                println(monstersToGet.length)
+                //now loop though those new arrays
+                for(i<-0 until monstersToGet.length){
+                    val currMonId = monstersToGet(i)
+                    val monsterConn = connection.createStatement()
+                    val monQuery = s"SELECT * FROM monsters WHERE monsterid = $currMonId"
+                    monsterConn.execute(monQuery)
+                    val res = monsterConn.getResultSet()
+                    while(res.next()){
+                        val tempID = res.getInt("monsterID")
+                        val tempName = res.getString("monsterName")
+                        val tempCr = res.getString("cr")
+                        val tempHealth = res.getInt("monsterHealth")
+                        val tempAtt = res.getInt("monsterAttack")
+                        val tempDef = res.getInt("monsterDefense") 
+                        val tempMonster = new Monster(tempID,tempName,tempHealth,tempAtt,tempDef, tempCr)
+                        tempMonsterMap += (tempMonster -> monstersAmounts(i))
+                    }
+                }
+                val generatorsNumber = splitGenData(0).split(",")
+                var finNum = generatorsNumber(1).toInt
+                val tempGen = new Generator(0,"Basic Accumulator", 1.5);
+                //this is lazy. I know its lazy. I dont have time to fix it.
+                val genMap = Map[Generator,Int](tempGen -> finNum)
+                player.createDungeon(tempMonsterMap, genMap, energy)
+                println("Dungeon loaded. Welcome back, Dungeon Keeper.")
             }
         }
+//================================================================================================================================
+        //this is where the actual game starts lol
+        var userInput = ""
+        do{
+            //ok look. This is rough. I'm not goikng to be doing much input sanatization, if any.
+            //im just going to do the bare minimun to get the requirment and to satisfy me.
+            println("Please input a command. Availible commands: \"tick\", \"view\", \"save\", \"delete\", and \"quit\".")
+            userInput = readLine("> ")
+            userInput.toLowerCase match {
+                case "view" => println(player.getDungeonStatus())
+                case "save" => {
+                                val monsterDat = "\"" + player.formatMonstersForSaving() + "\""
+                                val genDat = "\"" + player.formatGeneratorForSaving() + "\""
+                                val energyLevel = player.getEnergyLevel()
+                                val tempIDagain = player.getId()
+                                val queryf = s"UPDATE dungeondata SET monsterdata = $monsterDat, generators = $genDat, energy= $energyLevel WHERE dungeonID = $tempIDagain"
+                                //create a new statement
+                                val updateNew = connection.createStatement()
+                                updateNew.execute(queryf)}
+                case "delete" =>{
+                                    println("WARNING: this will PERMANTENTLY DELETE your save and account. Proceed? ")
+                                    val check = readLine("[Y/n]:")
+                                    check.toLowerCase() match {
+                                        case "y"=> {
+                                            println("deleteing account. Program will close after deletion.")
+                                            val playerid = player.getId()
+                                            val deleteQuery = s"DELETE FROM dungeondata WHERE dungeonid= $playerid"
+                                            val deleteQuery2 = s"DELETE FROM playerdata WHERE id = $playerid"
+                                            val deletion = connection.createStatement()
+                                            deletion.execute(deleteQuery)
+                                            deletion.execute(deleteQuery2)
+                                            connection.close()
+                                            sys.exit(0)
+                                        }
+                                        case _ => println("Not deleting account.")
+                                    }
+                                }
+                case "quit" => {println("Quiting...");connection.close(); sys.exit(0)}
+                case "tick" => {
+                                player.tick()
+                                }
+            }
+
+        }while(userInput != "quit")
         
         //close the connections at the end of the program.
         connection.close()
